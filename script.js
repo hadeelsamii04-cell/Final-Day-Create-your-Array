1dcabdf25c660aa0e4bdb8ded6e756a35f4d9748
hadeelsamii04-cell <hadeelsamii04@gmail.com>
2025-08-26 15:26:54 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // Create a empty array to store the names here 
 
+let names = [];
+
 
 // create a function that can add names to the array and print it in the list here 
+
+function addName(){
+
+    let inputVal = document.getElementById("name").value;
+    names.push(inputVal);
+    [inputVal].forEach(element => {
+        document.getElementById("namesList").innerHTML += "<li>" + element + "</li>"
+    });
+
+    console.log(names);
+
+}
+
+function deleteName(){
+    names.pop();
+    let remove = document.getElementById("namesList");
+    remove.removeChild(remove.lastChild)
+
+    console.log("names");
+}
 
 
 
